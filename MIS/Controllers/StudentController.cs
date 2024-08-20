0011084814d0b97547d9b78b8247e02e9515c468
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2024-08-20 17:04:19 +0530

    update

---
+++ MIS/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MIS.Controllers
+{
+    [Route("api/uni")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly
+    }
+}
